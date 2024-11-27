358e616eb44c82ac36487117f75c56ff2fcc360b
Paulojunioor <paulojunior201818@gmail.com>
2024-11-27 09:40:57 -0300

    Add files via upload

---
+++ paulo book5/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'automoveis',
+    'qual é a moto mais rapida do mundo?',
+    'NTT420-RR'
+)
+
+criaCartao(
+    'universidade',
+    'qual é a faculdade mais cobiçada ?',
+    'é a harvard university'
+)
+
+criaCartao(
+    'video game',
+    'qual é o videogame mais comprado?',
+    'é o playstation 5'
+)
+
+criaCartao(
+    'jogo',
+    'qual o jogo mais jogado?',
+    'o jogo é o gta 5 '
+)
